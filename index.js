5b99a673f37935ed6f2334c0acbb49ae03d04116
tingxun wang <wang.tingxu@northeastern.edu>
2025-11-20 00:33:30 -0500

    Fixed error

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import db from "./Kambaz/Database/index.js";
 import UserRoutes from "./Kambaz/Users/routes.js";
-import "dotenv/config";
 import session from "express-session";
 import CourseRoutes from "./Kambaz/Courses/routes.js";
 import ModulesRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentsRoutes from "./Kambaz/Assignments/routes.js";
 import EnrollmentsRoutes from "./Kambaz/Enrollments/routes.js";
+import "dotenv/config";
 
 const app = express();
 
 app.use(
     cors({
+        origin: process.env.CLIENT_URL || "https://kambaz-next-js5.vercel.app",
         credentials: true,
-        origin: process.env.CLIENT_URL || "http://localhost:3000",
     })
 );
 
@@ -24,16 +24,15 @@ const sessionOptions = {
     secret: process.env.SESSION_SECRET || "kambaz",
     resave: false,
     saveUninitialized: false,
+    cookie: {
+        sameSite: "none",
+        secure: true,
+        path: "/",
+    },
 };
 
 if (process.env.SERVER_ENV !== "development") {
     sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-        sameSite: "none",
-        secure: true,
-        domain: ".onrender.com",
-        path: "/",
-    };
 }
 
 app.use(session(sessionOptions));
@@ -44,7 +43,6 @@ CourseRoutes(app, db);
 ModulesRoutes(app, db);
 AssignmentsRoutes(app, db);
 EnrollmentsRoutes(app, db);
-
 Hello(app);
 Lab5(app);
 
